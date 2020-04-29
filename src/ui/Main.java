c3da5191bbc527fb558a3d9dea035b4abb1e4134
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-04-28 22:45:59 -0500

    Imagenes para disenio botones

---
+++ src/ui/Main.java
@@ -0,0 +1,43 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+launch(args);
+	}
+	
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("//.fxml"));
+
+		//fxmlLoader.setController(RsGUI);
+
+		Parent root = fxmlLoader.load();
+
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("imgns/iconos.css").toExternalForm());
+		primaryStage.setScene(scene);
+		
+		primaryStage.setTitle("All stock");
+		//RsGUI.loadSurvey(null);
+		primaryStage.show();
+		
+		
+	
+		
+
+	}
+	
+	
+
+}
