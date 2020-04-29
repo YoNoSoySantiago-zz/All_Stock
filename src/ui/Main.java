85b616784a78c21ed676cc76417cf18effdfaebf
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-04-29 00:17:28 -0500

    Menu de opciones y login
    Co-Authored-By: Dev.Pachon <dev-pachon@users.noreply.github.com>

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,43 +1,34 @@
 package ui;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
-	
-	
-	
+
+	private AllstockGUI allstockgui;
+
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage stage) throws Exception {
 
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
-
-		//fxmlLoader.setController(RsGUI);
-
-		Parent root = fxmlLoader.load();
+		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 
 		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("imgns/iconos.css").toExternalForm());
-		primaryStage.setScene(scene);
-		
-		primaryStage.setTitle("All stock");
-		//RsGUI.loadSurvey(null);
-		primaryStage.show();
-		
-		
-	
-		
+		scene.getStylesheets().add(getClass().getResource("iconos.css").toExternalForm());
+		stage.setScene(scene);
+		stage.setTitle("All Stock");
+		stage.show();
 
 	}
-	
-	
 
 }
