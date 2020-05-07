db30f926bed87dd98db2108cafd82cbd6884a84a
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-05-06 23:31:37 -0500

    prueba sesion en saros
    Co-Authored-By: Dev.Pachon <dev-pachon@users.noreply.github.com>

--- src/model/AllStock.java
+++ src/model/AllStock.java
@@ -1,6 +1,42 @@
 package model;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class AllStock {
-    
+	
+	//RELACIONES
+   
+	private SortedSet<User> users;
+	
+// agregar usuario en orden	
+	public AllStock() {
+		
+		Comparator c = new comparatorUser();
+		
+		users = new TreeSet<User>();
+		
+		
+		
+		
+	}
+	
+	
+	public void addUser(String id, String name, String idType, String password, String userType){
+		
+	}
+	
+	
+	//metodo para agregar al diagrama
+	
+	public void removeUser(String id) {
+		
+		
+	}
+
 
 }
