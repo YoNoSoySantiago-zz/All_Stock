e8bdaa85bf8e911db83f13cf68bc516ab192a7bf
YoNoSoySantiago <santiago.hurtado1@hotmail.com>
2020-05-17 20:38:58 -0500

    eo eo eoeo

--- src/CustomExceptions/AlreadyProductExistException.java
+++ src/CustomExceptions/AlreadyProductExistException.java
@@ -1,17 +1,19 @@
 package CustomExceptions;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import model.Product;
 
 public class AlreadyProductExistException extends Exception{
 	
 	public AlreadyProductExistException() {
 		
-	
-		
-		super( "EL PRODUCTO YA EXISTE");
-	
-		
+			Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("ERROR");
+		alert.setHeaderText("ALL STOCK");
+		alert.setContentText("EL PRODUCTO YA EXISTE ");
 
-		
+		alert.showAndWait();
+	
 	}
 
 }
